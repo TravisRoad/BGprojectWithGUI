46843c823ef07da70b9dc15a0cbeb2685f49c83e
Travis <lxy_montage@163.com>
2020-10-05 23:10:49 +0800

    new class but seemed useless

--- src/transport/Client.java
+++ src/transport/Client.java
@@ -1,39 +1,66 @@
 package transport;
 
-import java.io.IOException;
+import org.jetbrains.annotations.NotNull;
+
+import java.io.*;
 import java.net.Socket;
 
+/**
+ * 用于客户端与服务端通信
+ * @author Travis
+ */
 public class Client {
-    public Socket socket = null;
+    public Socket client = null;
     private final int DEFAULT_PORT = 1100;
     private final String DEFAULT_IP = "127.0.0.1";
+    private final char END_CHAR = '#';
+    private InputStream is;
 
-    public Client(){
-        try{
-            socket = new Socket(DEFAULT_IP,DEFAULT_PORT);
-        }catch (IOException e){
-            e.printStackTrace();
-        }
-    }
+    public Client(){}
 
-    public Client(String ip, int port){
-        try{
-            socket = new Socket(ip,port);
-        }catch (IOException e){
-            e.printStackTrace();
-        }
+    public boolean connect(){
+        return connect(DEFAULT_IP,DEFAULT_PORT);
     }
 
     public boolean connect(String ip, int port){
         boolean flag = false;
         try{
-            socket = new Socket(ip,port);
+            client = new Socket(ip,port);
+            InputStream is = client.getInputStream();
             flag = true;
         }catch (IOException e){
             e.printStackTrace();
         }finally {
             return flag;
         }
+    }
+
+    public String readStr(){
+        int len;
+        String recvMsg = "";
+
+        try {
+            byte[] buffer = new byte[1024];
+            while((len = is.read(buffer))!= -1){
+                recvMsg = recvMsg.concat(new String(buffer, 0, len));
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return recvMsg;
+    }
+
+    public boolean writeStr(@NotNull String str){
+        boolean flag = false;
+        try {
+            OutputStream out = client.getOutputStream();
+            out.write(str.getBytes("utf-8"));
+            flag = true;
+            client.shutdownOutput();
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+        return flag;
     }
 
 }
--- src/transport/Server.java
+++ src/transport/Server.java
@@ -1,4 +1,44 @@
 package transport;
 
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class Server {
+    ServerSocket service;
+    private final int DEFAULT_PORT = 1100;
+    private final int MAX_CLIENT_CNT = 5;
+    private final String DEFAULT_IP = "127.0.0.1";
+
+    public void start(){
+        try{
+            InetAddress address = InetAddress.getByName(DEFAULT_IP);
+            Socket connect = null;
+            ExecutorService pool = Executors.newFixedThreadPool(MAX_CLIENT_CNT);
+            service = new ServerSocket(DEFAULT_PORT,MAX_CLIENT_CNT,address);
+            while(true){
+                connect = service.accept();
+                //创建一个任务,放入线程池等待运行
+                ServiceTask serviceTask = new ServiceTask(connect);
+                pool.execute(serviceTask);
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    class ServiceTask implements Runnable{
+        private Socket socket;
+
+        ServiceTask(Socket socket){
+            this.socket = socket;
+        }
+
+        @Override
+        public void run() {
+
+        }
+    }
 }
