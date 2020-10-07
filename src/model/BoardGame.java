317d91a4b1f05e8147c0e01f7d65ca0aff191f45
Travis <lxy_montage@163.com>
2020-10-07 20:24:30 +0800

    new line in boardgame.java

--- src/model/BoardGame.java
+++ src/model/BoardGame.java
@@ -1,6 +1,10 @@
 package model;
 
+import java.util.ArrayList;
+
 public class BoardGame {
     private long ID;
     private long rank;
+    private String name;
+    private ArrayList<String> tags;
 }
