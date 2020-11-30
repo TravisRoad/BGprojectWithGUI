a312b2761e3dd772824d9675e611ac6fe25808dd
RiddMa <riddma@qq.com>
2020-11-30 22:32:28 +0800

    GameEntry

--- src/sample/test.java
+++ src/sample/test.java
@@ -9,8 +9,10 @@ import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import view.GameEntry;
 
 public class test extends Application {
 
@@ -25,23 +27,9 @@ public class test extends Application {
 
         Button addButton = new Button("Add");
         addButton.setOnAction(e -> {
-            AnchorPane anchorPane = new AnchorPane();
-            String style = String.format("-fx-background: rgb(%d, %d, %d);" +
-                            "-fx-background-color: -fx-background;",
-                    rng.nextInt(256),
-                    rng.nextInt(256),
-                    rng.nextInt(256));
-            anchorPane.setStyle(style);
-            Label label = new Label("Pane " + (content.getChildren().size() + 1));
-            AnchorPane.setLeftAnchor(label, 5.0);
-            AnchorPane.setTopAnchor(label, 5.0);
-            Button button = new Button("Remove");
-            button.setOnAction(evt -> content.getChildren().remove(anchorPane));
-            AnchorPane.setRightAnchor(button, 5.0);
-            AnchorPane.setTopAnchor(button, 5.0);
-            AnchorPane.setBottomAnchor(button, 5.0);
-            anchorPane.getChildren().addAll(label, button);
-            content.getChildren().add(anchorPane);
+            String url = "https://media.st.dl.pinyuncloud.com/steam/apps/803330/capsule_616x353.jpg?t=1606513853";
+            HBox hBox = new GameEntry(url,"hh","intro",9.0);
+            content.getChildren().add(hBox);
         });
 
         Scene scene = new Scene(new BorderPane(scroller, null, null, addButton, null), 400, 400);
---
+++ src/view/GameEntry.java
@@ -0,0 +1,55 @@
+package view;
+
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+
+
+public class GameEntry extends HBox {
+    String imageURL;
+    String title;
+    String intro;
+    double rating;
+
+
+    public GameEntry(String imageURL, String title, String intro, double rating) {
+        super();
+        this.imageURL = imageURL;
+        this.title = title;
+        this.intro = intro;
+        this.rating = rating;
+        setLayout();
+    }
+
+    private void setLayout() {
+
+        Image image = new Image(imageURL);
+        ImageView imageView = new ImageView(image);
+        imageView.setPreserveRatio(true);
+        imageView.setFitHeight(100);
+
+        VBox vBox = new VBox();
+        Label titleLabel = new Label(title);
+        Label introLabel = new Label(intro);
+        introLabel.setWrapText(true);
+
+        titleLabel.setFont(Font.font(null, FontWeight.BOLD, 32));
+        introLabel.setFont(Font.font(null, 20));
+
+        vBox.getChildren().addAll(titleLabel, introLabel);
+
+        Label ratingLabel = new Label(String.format("%.1f", rating));
+        //TODO
+
+        this.getChildren().addAll(imageView, vBox, ratingLabel);
+        setHgrow(imageView, Priority.ALWAYS);
+        setHgrow(vBox, Priority.ALWAYS);
+        setHgrow(ratingLabel, Priority.ALWAYS);
+    }
+}
